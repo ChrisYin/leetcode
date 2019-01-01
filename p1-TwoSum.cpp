6f5e03e54dff6c027b82413dca300901a6f29344
Zhenguo <chrisyin.2vv@gmail.com>
2019-01-01 14:24:50 -0500

    submit success

--- p1-TwoSum.cpp
+++ p1-TwoSum.cpp
@@ -4,29 +4,29 @@ using namespace std;
 #include <algorithm>
 #include <iostream>
 
-class Solution {
+//using sort
+/*class Solution {
 public:
     vector<int> twoSum(vector<int> &nums, int target) {
-        //input elements
-        for(vector<int>::const_iterator it = nums.begin(); it != nums.end(); it++){
-            printf("%d ", nums[it-nums.begin()]);
-        }
 
         vector<int> res;
 
-        sort(nums.begin(), nums.end());
+        vector<int> temp = nums;
+
+        sort(temp.begin(), temp.end());
 
         int counterPart;
         vector<int>::iterator it;
-        for (int i = 0; i <= nums.size(); i++) {
-            if (nums[i] >= target) {
+        for (int i = 0; i <= temp.size(); i++) {
+            if (temp[i] >= target) {
                 break;
             }
-            counterPart = target - nums[i];
-            it = find(nums.begin(), nums.end(), counterPart);
-            if (it != nums.end()) {
-                res.push_back(i);
-                res.push_back(it - nums.begin());
+            counterPart = target - temp[i];
+
+            it = find(temp.begin()+i+1, temp.end(), counterPart);
+            if (it != temp.end()) {
+                res.push_back(find(nums.begin(), nums.end(), temp[i]) - nums.begin());
+                res.push_back(find(nums.begin(), nums.end(), temp[it - temp.begin()]) - nums.begin());
                 return res;
             } else {
                 continue;
@@ -34,14 +34,47 @@ public:
         }
         return res;
     }
+};*/
+
+//not using sort
+class Solution {
+public:
+    vector<int> twoSum(vector<int> &nums, int target) {
+        vector<int> tempNums = nums;
+        vector<int> res;
+        vector<int>::iterator it;
+        for(int i = nums.size()-1 ; i >= 0; i--){
+            int counterPartNum = target - nums[i];
+            tempNums.pop_back();
+            //printf("find vector: ");
+            for(int j = 0; j < tempNums.size(); j++){
+                //printf("%d ", tempNums[j]);
+            }
+            //("\n");
+            //printf("find num: %d\n", counterPartNum);
+            it = find(tempNums.begin(), tempNums.end(), counterPartNum);
+            if (it != tempNums.end()) {
+                res.push_back(i);
+                res.push_back(it - tempNums.begin());
+                //printf("res: %d %d\n", i, it - tempNums.begin());
+                return res;
+            }
+        }
+        return res;
+    }
 };
 
 int main(){
-    vector<int> testArray = {2, 7, 11, 15};
+    vector<int> testArray = {2, 7, 11, 15, 3 , 5, 10};
     int target = 26;
+    //input elements
+    for(int i = 0; i < testArray.size(); i++){
+        printf("%d ", testArray[i]);
+    }
+    printf("\n");
+
     Solution test;
     vector<int> res = test.twoSum(testArray, target);
-    printf("%d\n", res[0]);
     printf("[%d %d]: %d + %d = %d\n", res[0], res[1], testArray[res[0]], testArray[res[1]], target);
     return 0;
 }
