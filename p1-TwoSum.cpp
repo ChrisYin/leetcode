bb1dab4a975a86548f48806230ad8cec165acfb2
Zhenguo Yin <chrisyin.2vv@gmail.com>
2019-01-01 13:29:24 -0500

    running success

--- p1-TwoSum.cpp
+++ p1-TwoSum.cpp
@@ -2,12 +2,20 @@ using namespace std;
 
 #include <vector>
 #include <algorithm>
+#include <iostream>
 
 class Solution {
 public:
     vector<int> twoSum(vector<int> &nums, int target) {
+        //input elements
+        for(vector<int>::const_iterator it = nums.begin(); it != nums.end(); it++){
+            printf("%d ", nums[it-nums.begin()]);
+        }
+
+        vector<int> res;
+
         sort(nums.begin(), nums.end());
-        vector<int> res(0, 0);
+
         int counterPart;
         vector<int>::iterator it;
         for (int i = 0; i <= nums.size(); i++) {
@@ -17,8 +25,8 @@ public:
             counterPart = target - nums[i];
             it = find(nums.begin(), nums.end(), counterPart);
             if (it != nums.end()) {
-                res[0] = i;
-                res[1] = it - nums.begin();
+                res.push_back(i);
+                res.push_back(it - nums.begin());
                 return res;
             } else {
                 continue;
@@ -26,9 +34,14 @@ public:
         }
         return res;
     }
-
-    void main(){
-
-    }
-
 };
+
+int main(){
+    vector<int> testArray = {2, 7, 11, 15};
+    int target = 26;
+    Solution test;
+    vector<int> res = test.twoSum(testArray, target);
+    printf("%d\n", res[0]);
+    printf("[%d %d]: %d + %d = %d\n", res[0], res[1], testArray[res[0]], testArray[res[1]], target);
+    return 0;
+}
