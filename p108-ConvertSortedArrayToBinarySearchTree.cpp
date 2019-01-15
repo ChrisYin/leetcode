90f8249e15ccbdb4b470549c520a24ff5b23ef5e
Zhenguo Yin <chrisyin.2vv@gmail.com>
2019-01-14 23:46:24 -0500

    p108 submit success

--- p108-ConvertSortedArrayToBinarySearchTree.cpp
+++ p108-ConvertSortedArrayToBinarySearchTree.cpp
@@ -23,12 +23,44 @@ struct TreeNode {
     TreeNode(int x) : val(x), left(NULL), right(NULL) {};
 };
 
+//my solution: 12ms 99.58%
 class Solution {
+private:
+    void createMidNode(vector<int> &nums, int start, int end, TreeNode *parentNode, bool isLeft){
+        //cout<<"start: "<<start<<"end: "<<end<<endl;
+
+        if(end < start)
+            return;
+        int mid = (start + end)/2;
+        //cout<<"mid: "<<mid<<endl;
+        TreeNode *childNode = new TreeNode(nums[mid]);
+        //cout<<"new node"<<childNode<<":  "<<childNode->val<<endl;
+        if(isLeft){
+            parentNode->left = childNode;
+        }else{
+            parentNode->right = childNode;
+        }
+        createMidNode(nums, start, mid-1, childNode, true);
+        createMidNode(nums, mid+1, end, childNode, false);
+    }
 public:
     TreeNode *sortedArrayToBST(vector<int> &nums) {
         if (nums.empty())
             return NULL;
-
+        TreeNode *rootNode = new TreeNode(nums[nums.size()/2]);
+        //cout<<"new node "<<rootNode<<":  "<<rootNode->val<<endl;
+        createMidNode(nums, 0, nums.size()/2 -1, rootNode, true);
+        createMidNode(nums, nums.size()/2+1, nums.size()-1, rootNode, false);
+        return rootNode;
     }
 };
 
+int main(){
+    vector<int> testArray = {-10,-3,0,5,9, 11};
+    Solution test;
+    TreeNode *res = test.sortedArrayToBST(testArray);
+
+
+
+    return 0;
+}
