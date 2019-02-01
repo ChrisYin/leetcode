74e51ee81895675e8d74c7424fb576ab7d80a478
Zhenguo <chrisyin.2vv@gmail.com>
2019-01-31 23:58:54 -0500

    add p743.cpp

---
+++ p743-NetworkDelayTime.cpp
@@ -0,0 +1,23 @@
+//
+// Created by zy234 on 1/31/19.
+//
+/*
+ * How to calculate the delay time??
+ *
+ * directed edge?
+ *
+ * graph algorithm: don't know
+ *
+ * using hashmap to record the delay time of each node
+ * then loop to find the max delay time.
+ */
+using namespace std;
+
+#include <vector>
+
+class Solution {
+public:
+    int networkDelayTime(vector<vector<int>>& times, int N, int K) {
+
+    }
+};
